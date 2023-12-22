28dbf7340f19c1e19736df104ec024984423d61b
John Doe <nurikk@users.noreply.github.com>
2023-12-22 20:37:42 +0000

    Refactor prompt generation to use a list and join the elements

--- src/prompt_generator.py
+++ src/prompt_generator.py
@@ -1,3 +1,9 @@
 def generate_prompt(data: dict[str, str]) -> str:
-    prompt = f"Generate a postcard for my {data['relationship']} for {data['reason']}. He is {data['description']}. Depict {data['depiction']}. Use {data['style']} style. Add appropriate text with congratulations with {data['reason']}."
-    return prompt
+    prompt = [
+        f"Generate a postcard for my {data['relationship']} for {data['reason']}.",
+        f"He is {data['description']}.",
+        f"Depict {data['depiction']}.",
+        f"Use {data['style']} style."
+        f"Add appropriate text with congratulations with {data['reason']}."
+    ]
+    return " ".join(prompt)
