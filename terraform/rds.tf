0437e75abc81c22064fe8904c0180332a97c7bf6
John Doe <nurikk@users.noreply.github.com>
2023-12-23 18:05:39 +0000

    Update RDS engine version from 15.5-R1 to 16.1

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -2,9 +2,9 @@ resource "aws_db_instance" "db" {
   allocated_storage           = 20
   db_name                     = "tele-bot"
   engine                      = "postgres"
-  engine_version              = "15.5-R1"
+  engine_version              = "16.1"
   instance_class              = "db.t3.micro"
   username                    = "bot"
   manage_master_user_password = true
-  parameter_group_name = "default.postgres15"
+  parameter_group_name = "default.postgres16"
 }
