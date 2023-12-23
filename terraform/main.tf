3685a3865de21fcf5fb8a1632956eb09fffdf9a5
John Doe <nurikk@users.noreply.github.com>
2023-12-23 16:29:26 +0000

    Add ECR lifecycle policy for image cleanup

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,27 @@ resource "aws_ecr_repository" "app_ecr_repo" {
   name = "tele-bot-repo"
 }
 
+resource "aws_ecr_lifecycle_policy" "cleanup_policy" {
+  repository = aws_ecr_repository.app_ecr_repo.name
+  policy = jsonencode(
+    {
+      "rules" : [
+        {
+          "rulePriority" : 1,
+          "description" : "Expire images older than 2 days",
+          "selection" : {
+            "tagStatus" : "untagged",
+            "countType" : "sinceImagePushed",
+            "countUnit" : "days",
+            "countNumber" : 2
+          },
+          "action" : {
+            "type" : "expire"
+          }
+        }
+      ]
+  })
+}
 resource "aws_ecs_cluster" "cluster" {
   name = "tele-bot-cluster"
 }
