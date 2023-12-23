9aaa4c2c8c07ae44d7755970c06ae242c45711f3
John Doe <nurikk@users.noreply.github.com>
2023-12-23 18:07:08 +0000

    Fix database name in RDS configuration

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,6 +1,6 @@
 resource "aws_db_instance" "db" {
   allocated_storage           = 20
-  db_name                     = "tele-bot"
+  db_name                     = "telebot"
   engine                      = "postgres"
   engine_version              = "16.1"
   instance_class              = "db.t3.micro"
