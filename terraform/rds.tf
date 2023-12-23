2232b3920c8625e54450a6d0cb88e63d754b3933
John Doe <nurikk@users.noreply.github.com>
2023-12-23 22:36:33 +0000

    Update random_password special character configuration

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,7 +1,6 @@
 resource "random_password" "db_password" {
   length           = 16
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
+  special          = false
 }
 
 resource "aws_db_instance" "db" {
