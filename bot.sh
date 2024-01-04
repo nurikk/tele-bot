e421734cabc34388acba979bd3cf08025c199bbf
John Doe <nurikk@users.noreply.github.com>
2024-01-04 22:41:41 +0000

    Update Dockerfile and bot.sh scripts

--- bot.sh
+++ bot.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-SELF_IP=$(curl https://ipinfo.io/ip)
-echo "Self IP: $SELF_IP"
-export SELF_IP
 aerich upgrade
 NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program python -m src.main
