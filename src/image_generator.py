359e1f5760c8bf81279dc3b796c44358294c0e51
John Doe <nurikk@users.noreply.github.com>
2024-01-07 16:46:14 +0000

    Add async_run method to ReplicateGenerator

--- src/image_generator.py
+++ src/image_generator.py
@@ -5,6 +5,10 @@ from typing import Literal
 
 from openai import AsyncOpenAI
 import replicate
+from replicate import identifier
+from replicate.exceptions import ModelError
+from replicate.run import _make_output_iterator
+from replicate.version import Versions
 
 
 class ImageGenerator(abc.ABC):
@@ -26,26 +30,43 @@ class ReplicateGenerator(ImageGenerator):
         self.client = replicate.Client(api_token=api_token)
         self.api_token = api_token
 
+    async def async_run(self,
+                        ref,
+                        input,
+                        **params,
+                        ):
+        version, owner, name, version_id = identifier._resolve(ref)
+
+        if version or version_id:
+            prediction = await self.client.predictions.async_create(
+                version=(version or version_id), input=input or {}, **params
+            )
+        elif owner and name:
+            prediction = await self.client.models.predictions.async_create(
+                model=(owner, name), input=input or {}, **params
+            )
+        else:
+            raise ValueError(
+                f"Invalid argument: {ref}. Expected model, version, or reference in the format owner/name or owner/name:version"
+            )
+
+        if not version and (owner and name and version_id):
+            version = Versions(self.client, model=(owner, name)).get(version_id)
+
+        if version and (iterator := _make_output_iterator(version, prediction)):
+            return iterator
+
+        while prediction.status not in ["succeeded", "failed", "canceled"]:
+            await asyncio.sleep(self.client.poll_interval)
+            prediction.reload()
+
+        if prediction.status == "failed":
+            raise ModelError(prediction.error)
+
+        return prediction.output
+
     async def generate(self, prompt: str, images_count: int = 1) -> list[str]:
-        # return await self.client.async_run(
-        #     "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
-        #     input={
-        #         "width": 768,
-        #         "height": 768,
-        #         "prompt": prompt,
-        #         "refine": "expert_ensemble_refiner",
-        #         "scheduler": "K_EULER",
-        #         "lora_scale": 0.6,
-        #         "num_outputs": 4,
-        #         "guidance_scale": 7.5,
-        #         "apply_watermark": False,
-        #         "high_noise_frac": 0.8,
-        #         "negative_prompt": "",
-        #         "prompt_strength": 0.8,
-        #         "num_inference_steps": 25
-        #     }
-        # )
-        futures = [self.client.async_run(
+        futures = [self.async_run(
             "playgroundai/playground-v2-1024px-aesthetic:42fe626e41cc811eaf02c94b892774839268ce1994ea778eba97103fe1ef51b8",
             input={
                 "width": 1024,
