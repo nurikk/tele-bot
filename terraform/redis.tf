49861752af1f1d89bb9cc48458494c79ea34ad7a
John Doe <nurikk@users.noreply.github.com>
2024-01-12 17:20:13 +0000

    Update Redis node type to cache.t3.micro

--- terraform/redis.tf
+++ terraform/redis.tf
@@ -1,7 +1,7 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "telebot-redis"
   engine               = "redis"
-  node_type            = "cache.t4g.micro"
+  node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis7"
   engine_version       = "7.1"
