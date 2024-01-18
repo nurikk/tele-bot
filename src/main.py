cf38cb03a5910c445cb3c26bd114ee9785b6dbf6
John Doe <nurikk@users.noreply.github.com>
2024-01-18 20:10:24 +0000

    Add ImageProxy to main.py

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ from openai import AsyncOpenAI
 from src import db
 from src.commands import card_bot_command, stop_bot_command, calendar_bot_command
 from src.image_generator import ReplicateGenerator
-from src.img import ImageOptim
+from src.img import ImageOptim, ImageProxy
 from src.message_handlers.broadcaster import register as register_broadcast_handler
 from src.message_handlers.card import register as register_card_handler
 from src.message_handlers.img import register as register_img_handler
@@ -83,11 +83,10 @@ if __name__ == "__main__":
                     settings=settings,
                     s3_uploader=S3Uploader(aws_access_key_id=settings.aws_access_key_id, aws_secret_access_key=settings.aws_secret_access_key,
                                            aws_region=settings.aws_region, s3_bucket_name=settings.s3_bucket_name),
-                    image_proxy=ImageOptim(account_id=settings.imageoptim_account_id),
-                    # image_proxy=ImageProxy(imgproxy_hostname=settings.imgproxy_hostname,
-                    #                        imgproxy_port=settings.imgproxy_port,
-                    #                        imgproxy_key=settings.imgproxy_key,
-                    #                        imgproxy_salt=settings.imgproxy_salt)
+                    # image_proxy=ImageOptim(account_id=settings.imageoptim_account_id),
+                    image_proxy=ImageProxy(imgproxy_hostname=settings.imgproxy_hostname,
+                                           imgproxy_key=settings.imgproxy_key,
+                                           imgproxy_salt=settings.imgproxy_salt)
                     )
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main(dispatcher=dp, telegram_bot_token=settings.telegram_bot_token, db_url=settings.db_url))
