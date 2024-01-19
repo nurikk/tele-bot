f2bc9d4f59c5307f7a5888a18b308d574c14dc7f
John Doe <nurikk@users.noreply.github.com>
2024-01-19 14:50:56 +0000

    Update CodeBuild configuration

--- terraform/codepipeline.tf
+++ terraform/codepipeline.tf
@@ -1,169 +1,169 @@
-#resource "aws_codepipeline" "codepipeline" {
-#  name     = "telebot-code-pipeline"
-#  role_arn = aws_iam_role.codepipeline_role.arn
-#
-#  artifact_store {
-#    location = aws_s3_bucket.codepipeline_bucket.bucket
-#    type     = "S3"
-#  }
-#
-#  stage {
-#    name = "Source"
-#
-#    action {
-#      name             = "Source"
-#      category         = "Source"
-#      owner            = "AWS"
-#      provider         = "CodeStarSourceConnection"
-#      version          = "1"
-#      output_artifacts = ["source_output"]
-#
-#      configuration = {
-#        ConnectionArn    = aws_codestarconnections_connection.example.arn
-#        FullRepositoryId = var.CODEPIPELINE_GITHUB_REPO
-#        BranchName       = var.GITHUB_BRANCH
-#      }
-#    }
-#  }
-#
-#  stage {
-#    name = "Build"
-#
-#    action {
-#      name             = "Build"
-#      category         = "Build"
-#      owner            = "AWS"
-#      provider         = "CodeBuild"
-#      input_artifacts  = ["source_output"]
-#      output_artifacts = ["build_output"]
-#      version          = "1"
-#
-#      configuration = {
-#        ProjectName = aws_codebuild_project.codebuild.name
-#      }
-#    }
-#  }
-#
-#  stage {
-#    name = "Deploy"
-#
-#    action {
-#      name            = "Deploy"
-#      category        = "Deploy"
-#      owner           = "AWS"
-#      provider        = "ECS"
-#      input_artifacts = ["build_output"]
-#      version         = "1"
-#
-#      configuration = {
-#        ClusterName = aws_ecs_cluster.cluster.name
-#        FileName    = "imagedefinitions.json"
-#        ServiceName = aws_ecs_service.app_service.name
-#      }
-#    }
-#  }
-#}
-#
-#resource "aws_codestarconnections_connection" "example" {
-#  name          = "example-connection"
-#  provider_type = "GitHub"
-#}
-#
-#resource "aws_s3_bucket" "codepipeline_bucket" {
-#  bucket = "telebot-codepipeline-bucket"
-#}
-#
-#resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_pab" {
-#  bucket = aws_s3_bucket.codepipeline_bucket.id
-#
-#  block_public_acls       = true
-#  block_public_policy     = true
-#  ignore_public_acls      = true
-#  restrict_public_buckets = true
-#}
-#
-#data "aws_iam_policy_document" "assume_role" {
-#  statement {
-#    effect = "Allow"
-#
-#    principals {
-#      type        = "Service"
-#      identifiers = ["codepipeline.amazonaws.com"]
-#    }
-#
-#    actions = ["sts:AssumeRole"]
-#  }
-#}
-#
-#resource "aws_iam_role" "codepipeline_role" {
-#  name               = "codepipeline_role-telebot"
-#  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-#}
-#
-#data "aws_iam_policy_document" "codepipeline_policy" {
-#  statement {
-#    effect = "Allow"
-#
-#    actions = [
-#      "s3:GetObject",
-#      "s3:GetObjectVersion",
-#      "s3:GetBucketVersioning",
-#      "s3:PutObjectAcl",
-#      "s3:PutObject",
-#    ]
-#
-#    resources = [
-#      aws_s3_bucket.codepipeline_bucket.arn,
-#      "${aws_s3_bucket.codepipeline_bucket.arn}/*"
-#    ]
-#  }
-#
-#  statement {
-#    effect    = "Allow"
-#    actions   = ["codestar-connections:UseConnection"]
-#    resources = [aws_codestarconnections_connection.example.arn]
-#  }
-#
-#  statement {
-#    effect = "Allow"
-#
-#    actions = [
-#      "codebuild:BatchGetBuilds",
-#      "codebuild:StartBuild",
-#    ]
-#
-#    resources = ["*"]
-#  }
-#
-#  statement {
-#    effect  = "Allow"
-#    actions = [
-#      "codedeploy:CreateDeployment",
-#      "codedeploy:GetApplicationRevision",
-#      "codedeploy:GetApplication",
-#      "codedeploy:GetDeployment",
-#      "codedeploy:GetDeploymentConfig",
-#      "codedeploy:RegisterApplicationRevision",
-#      "ecr:*"
-#    ]
-#    resources = ["*"]
-#  }
-#
-#  statement {
-#    sid    = "AllowPassRole"
-#    effect = "Allow"
-#
-#    actions = ["iam:PassRole"]
-#
-#    resources = [
-#      aws_iam_role.ecsTaskExecutionRole.arn,
-##      "${aws_iam_role.task_role.arn}",
-#    ]
-#  }
-#}
-#
-#resource "aws_iam_role_policy" "codepipeline_policy" {
-#name = "codepipeline_policy"
-#role = aws_iam_role.codepipeline_role.id
-#policy = data.aws_iam_policy_document.codepipeline_policy.json
-#}
+resource "aws_codepipeline" "codepipeline" {
+  name     = "telebot-code-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        ConnectionArn    = aws_codestarconnections_connection.example.arn
+        FullRepositoryId = var.CODEPIPELINE_GITHUB_REPO
+        BranchName       = var.GITHUB_BRANCH
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.codebuild.name
+      }
+    }
+  }
+
+  #  stage {
+  #    name = "Deploy"
+  #
+  #    action {
+  #      name            = "Deploy"
+  #      category        = "Deploy"
+  #      owner           = "AWS"
+  #      provider        = "ECS"
+  #      input_artifacts = ["build_output"]
+  #      version         = "1"
+  #
+  #      configuration = {
+  #        ClusterName = aws_ecs_cluster.cluster.name
+  #        FileName    = "imagedefinitions.json"
+  #        ServiceName = aws_ecs_service.app_service.name
+  #      }
+  #    }
+  #  }
+}
+
+resource "aws_codestarconnections_connection" "example" {
+  name          = "example-connection"
+  provider_type = "GitHub"
+}
+
+resource "aws_s3_bucket" "codepipeline_bucket" {
+  bucket = "telebot-codepipeline-bucket"
+}
+
+resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_pab" {
+  bucket = aws_s3_bucket.codepipeline_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name               = "codepipeline_role-telebot"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "codepipeline_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObjectAcl",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.codepipeline_bucket.arn,
+      "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+    ]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["codestar-connections:UseConnection"]
+    resources = [aws_codestarconnections_connection.example.arn]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect  = "Allow"
+    actions = [
+      "codedeploy:CreateDeployment",
+      "codedeploy:GetApplicationRevision",
+      "codedeploy:GetApplication",
+      "codedeploy:GetDeployment",
+      "codedeploy:GetDeploymentConfig",
+      "codedeploy:RegisterApplicationRevision",
+      "ecr:*"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "AllowPassRole"
+    effect = "Allow"
+
+    actions = ["iam:PassRole"]
+
+    resources = [
+      aws_iam_role.ecsTaskExecutionRole.arn,
+      #      "${aws_iam_role.task_role.arn}",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name   = "codepipeline_policy"
+  role   = aws_iam_role.codepipeline_role.id
+  policy = data.aws_iam_policy_document.codepipeline_policy.json
+}
