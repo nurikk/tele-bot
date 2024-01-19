9761223dbc949e311bf9f73197b28a3d3f916f43
John Doe <nurikk@users.noreply.github.com>
2024-01-19 14:51:20 +0000

    Refactor codebuild.tf file

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -1,140 +1,140 @@
-data "aws_caller_identity" "current" {}
-
-
-resource "aws_iam_role" "codebuild" {
-  name = "telebot-codebuild-role"
-
-  assume_role_policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Effect" : "Allow",
-          "Principal" : {
-            "Service" : "codebuild.amazonaws.com"
-          },
-          "Action" : "sts:AssumeRole"
-        }
-      ]
-    })
-}
-
-resource "aws_iam_role_policy" "codebuild" {
-  role = aws_iam_role.codebuild.name
-
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Resource" : [
-          "*"
-        ],
-        "Action" : [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "s3:*"
-        ],
-        "Resource" : [
-          aws_s3_bucket.codepipeline_bucket.arn,
-          "${aws_s3_bucket.codepipeline_bucket.arn}/*"
-        ]
-      },
-      {
-        "Action" : [
-          "ecr:GetAuthorizationToken",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:PutImage",
-          "ecr:InitiateLayerUpload",
-          "ecr:UploadLayerPart",
-          "ecr:CompleteLayerUpload"
-        ],
-        "Resource" : "*",
-        "Effect" : "Allow"
-      },
-      {
-        "Action" : [
-          "ssm:GetParameters"
-        ],
-        "Resource" : "*",
-        "Effect" : "Allow"
-      }
-    ]
-  })
-}
-
-resource "aws_codebuild_project" "codebuild" {
-  name         = "telebot-codebuild-project"
-  service_role = aws_iam_role.codebuild.arn
-
-  artifacts {
-    type = "CODEPIPELINE"
-  }
-
-
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "CODEBUILD"
-    privileged_mode             = true
-
-    environment_variable {
-      name  = "DOCKER_BUILDKIT"
-      value = "1"
-    }
-
-    environment_variable {
-      name  = "AWS_ACCOUNT_ID"
-      value = data.aws_caller_identity.current.account_id
-    }
-    environment_variable {
-      name  = "IMAGE_REPO_NAME"
-      value = aws_ecr_repository.app_ecr_repo.name
-    }
-    environment_variable {
-      name  = "IMAGE_TAG"
-      value = "latest"
-    }
-
-    environment_variable {
-      name  = "REPOSITORY_URI"
-      value = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${aws_ecr_repository.app_ecr_repo.name}"
-    }
-
-    environment_variable {
-      name  = "CONTAINER_NAME"
-      value = local.telebot_container_name
-    }
-  }
-
-  logs_config {
-    cloudwatch_logs {
-      group_name  = aws_cloudwatch_log_group.logs.name
-      stream_name = "log-stream"
-    }
-  }
-
-  cache {
-    type  = "S3"
-    location = aws_s3_bucket.codepipeline_bucket.bucket
-    modes = ["LOCAL_DOCKER_LAYER_CACHE"]
-  }
-
-  source {
-    type      = "CODEPIPELINE"
-    #    location        = var.GITHUB_REPO
-    #    git_clone_depth = 1
-    buildspec = templatefile("${path.cwd}/buildspec.yml", {})
-  }
-
-  source_version = "master"
-}
+#data "aws_caller_identity" "current" {}
+#
+#
+#resource "aws_iam_role" "codebuild" {
+#  name = "telebot-codebuild-role"
+#
+#  assume_role_policy = jsonencode(
+#    {
+#      "Version" : "2012-10-17",
+#      "Statement" : [
+#        {
+#          "Effect" : "Allow",
+#          "Principal" : {
+#            "Service" : "codebuild.amazonaws.com"
+#          },
+#          "Action" : "sts:AssumeRole"
+#        }
+#      ]
+#    })
+#}
+#
+#resource "aws_iam_role_policy" "codebuild" {
+#  role = aws_iam_role.codebuild.name
+#
+#  policy = jsonencode({
+#    "Version" : "2012-10-17",
+#    "Statement" : [
+#      {
+#        "Effect" : "Allow",
+#        "Resource" : [
+#          "*"
+#        ],
+#        "Action" : [
+#          "logs:CreateLogGroup",
+#          "logs:CreateLogStream",
+#          "logs:PutLogEvents"
+#        ]
+#      },
+#      {
+#        "Effect" : "Allow",
+#        "Action" : [
+#          "s3:*"
+#        ],
+#        "Resource" : [
+#          aws_s3_bucket.codepipeline_bucket.arn,
+#          "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+#        ]
+#      },
+#      {
+#        "Action" : [
+#          "ecr:GetAuthorizationToken",
+#          "ecr:GetDownloadUrlForLayer",
+#          "ecr:BatchGetImage",
+#          "ecr:BatchCheckLayerAvailability",
+#          "ecr:PutImage",
+#          "ecr:InitiateLayerUpload",
+#          "ecr:UploadLayerPart",
+#          "ecr:CompleteLayerUpload"
+#        ],
+#        "Resource" : "*",
+#        "Effect" : "Allow"
+#      },
+#      {
+#        "Action" : [
+#          "ssm:GetParameters"
+#        ],
+#        "Resource" : "*",
+#        "Effect" : "Allow"
+#      }
+#    ]
+#  })
+#}
+#
+#resource "aws_codebuild_project" "codebuild" {
+#  name         = "telebot-codebuild-project"
+#  service_role = aws_iam_role.codebuild.arn
+#
+#  artifacts {
+#    type = "CODEPIPELINE"
+#  }
+#
+#
+#  environment {
+#    compute_type                = "BUILD_GENERAL1_SMALL"
+#    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+#    type                        = "LINUX_CONTAINER"
+#    image_pull_credentials_type = "CODEBUILD"
+#    privileged_mode             = true
+#
+#    environment_variable {
+#      name  = "DOCKER_BUILDKIT"
+#      value = "1"
+#    }
+#
+#    environment_variable {
+#      name  = "AWS_ACCOUNT_ID"
+#      value = data.aws_caller_identity.current.account_id
+#    }
+#    environment_variable {
+#      name  = "IMAGE_REPO_NAME"
+#      value = aws_ecr_repository.app_ecr_repo.name
+#    }
+#    environment_variable {
+#      name  = "IMAGE_TAG"
+#      value = "latest"
+#    }
+#
+#    environment_variable {
+#      name  = "REPOSITORY_URI"
+#      value = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${aws_ecr_repository.app_ecr_repo.name}"
+#    }
+#
+#    environment_variable {
+#      name  = "CONTAINER_NAME"
+#      value = local.telebot_container_name
+#    }
+#  }
+#
+#  logs_config {
+#    cloudwatch_logs {
+#      group_name  = aws_cloudwatch_log_group.logs.name
+#      stream_name = "log-stream"
+#    }
+#  }
+#
+#  cache {
+#    type  = "S3"
+#    location = aws_s3_bucket.codepipeline_bucket.bucket
+#    modes = ["LOCAL_DOCKER_LAYER_CACHE"]
+#  }
+#
+#  source {
+#    type      = "CODEPIPELINE"
+#    #    location        = var.GITHUB_REPO
+#    #    git_clone_depth = 1
+#    buildspec = templatefile("${path.cwd}/buildspec.yml", {})
+#  }
+#
+#  source_version = "master"
+#}
